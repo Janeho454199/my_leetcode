13f963ed6d7c2f6c4f6063dce2a4122fb4cc30ac
Janeho454199 <505122753@qq.com>
2022-06-14 12:29:48 +0800

    20220613

---
+++ list/566-重塑矩阵.py
@@ -0,0 +1,11 @@
+class Solution:
+    def matrixReshape(self, nums, r, c):
+        m, n = len(nums), len(nums[0])
+        if m * n != r * c:
+            return nums
+
+        ans = [[0] * c for _ in range(r)]
+        for x in range(m * n):
+            ans[x // c][x % c] = nums[x // n][x % n]
+
+        return ans
