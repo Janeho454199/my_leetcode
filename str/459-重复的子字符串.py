9f02f8714ac8cfac96ad768db225750bae497491
Janeho454199 <505122753@qq.com>
2022-07-26 17:24:39 +0800

    20220726

--- str/459-重复的子字符串.py
+++ str/459-重复的子字符串.py
@@ -7,3 +7,16 @@ class Solution:
         """
         return (s + s).find(s, 1) != len(s)
 
+    def repeatedSubstringPattern2(self, s: str) -> bool:
+        """
+        枚举
+        :param s:
+        :return:
+        """
+        n = len(s)
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:
+                if all(s[j] == s[j - i] for j in range(i, n)):
+                    return True
+        return False
+
